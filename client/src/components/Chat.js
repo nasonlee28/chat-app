6cb70c322ba5006ea73f323daa58a74ba7786f53
Nason <nason@shopback.com>
2024-01-15 23:01:33 +0800

    [POC] Implement chat room POC

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import io from 'socket.io-client';
 
 const socket = io(
@@ -7,31 +7,51 @@ const socket = io(
     : 'localhost:8080/',
 );
 
+const Message = ({ message }) => {
+  const isMe = message.socketId === socket.id;
+  return (
+    <>
+      <div className={isMe ? 'me-message-name' : 'other-message-name'}>
+        {message.socketId}
+      </div>
+      <div className={isMe ? 'me' : 'other'}>
+        <div>{message.message}</div>
+      </div>
+    </>
+  );
+};
+
 const Chat = () => {
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState('');
 
+  const messageEndRef = useRef(null);
+
   useEffect(() => {
+    console.log('registering callback');
     const callback = (message) => {
+      console.log('xx message', message);
+      console.log('xx socket', socket);
       setMessages((prevMessages) => {
         return [...prevMessages, message];
       });
     };
-    socket.on(
-      // "identity",
-      'events',
-      callback,
-    );
+    socket.on('chat', callback);
 
-    return () => socket.off('events', callback);
+    return () => {
+      socket.off('chat', callback);
+    };
   }, []);
 
+  /**
+   * Scroll to bottom of chat when new message is received
+   */
+  useEffect(() => {
+    messageEndRef.current?.scrollIntoView?.();
+  }, [messages]);
+
   const sendMessage = () => {
-    socket.emit(
-      // "identity",
-      'events',
-      newMessage,
-    );
+    socket.emit('chat', { socketId: socket.id, message: newMessage });
     setNewMessage('');
   };
 
@@ -39,17 +59,25 @@ const Chat = () => {
     <div className="background">
       <div className="chat">
         {messages.map((msg, index) => (
-          <div key={index}>{msg}</div>
+          <Message key={index} message={msg} />
         ))}
+        <div ref={messageEndRef} />
       </div>
-      <div>
+      <div className="edit-bar">
         <input
           className="input"
           type="text"
           value={newMessage}
           onChange={(e) => setNewMessage(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              sendMessage();
+            }
+          }}
         />
-        <button onClick={sendMessage}>Send</button>
+        <button className="send" onClick={sendMessage}>
+          Send
+        </button>
       </div>
     </div>
   );
--- src/modules/events/events.gateway.ts
+++ src/modules/events/events.gateway.ts
@@ -3,10 +3,7 @@ import {
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
-  WsResponse,
 } from '@nestjs/websockets';
-import { from, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { Server } from 'socket.io';
 
 @WebSocketGateway({
@@ -18,17 +15,8 @@ export class EventsGateway {
   @WebSocketServer()
   server: Server;
 
-  @SubscribeMessage('events')
-  findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
-    console.log('xx data', data);
-    return from([1, 2, 3]).pipe(
-      map((item) => ({ event: 'events', data: item })),
-    );
-  }
-
-  @SubscribeMessage('identity')
-  identity(@MessageBody() data: unknown): WsResponse<unknown> {
-    console.log('x data', data);
-    return { event: 'identity', data };
+  @SubscribeMessage('chat')
+  findAll(@MessageBody() data: any): any {
+    this.server.emit('chat', data);
   }
 }
